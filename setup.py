3144e30b2c176676e63ebf826c744579440d4357
Father Vlasie <33334298+fvlasie@users.noreply.github.com>
2026-07-27 23:07:53 -0700

    Adopt standard Frappe setup.py and MANIFEST.in structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
+
+from bloomerang_erpnext import __version__ as version
+
+setup(
+    name="bloomerang_erpnext",
+    version=version,
+    description="Bloomerang Integration for ERPNext",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
+)
